ed2753a5f8379b92b8e8e59bc66e531bbb387c5c
marina <marinarosilloruiz02@gmail.com>
2023-05-23 10:27:27 +0200

    Clase Vehiculo, atributos, constructor y getters y setters

--- Grupo2_Ejercicio2/src/concesionario/Vehiculo.java
+++ Grupo2_Ejercicio2/src/concesionario/Vehiculo.java
@@ -2,5 +2,51 @@ package concesionario;
 
 public class Vehiculo {
 
-	//primer commit
+	//Atributos
+	private String marca;
+	private String modelo;
+	private String anyo;
+	private double precio;
+	
+	//Constructor
+	public Vehiculo(String marca, String modelo, String anyo, double precio) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anyo = anyo;
+		this.precio = precio;
+	}
+
+	//Getters y Setters
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getAnyo() {
+		return anyo;
+	}
+
+	public void setAnyo(String anyo) {
+		this.anyo = anyo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 }
