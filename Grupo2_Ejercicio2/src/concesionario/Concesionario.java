a96502269f3e36da8216097c925a736b2bdc46a8
lola <lo.ruiz97@gmail.com>
2023-05-23 12:25:51 +0200

    Clase Concesionario

---
+++ Grupo2_Ejercicio2/src/concesionario/Concesionario.java
@@ -0,0 +1,21 @@
+package concesionario;
+
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Concesionario {
+	
+	//Atributos
+	private Set<Vehiculo> inventario;
+
+	//Constructor
+	public Concesionario(Set<Vehiculo> inventario) {
+		super();
+		this.inventario = new TreeSet<>();
+	}
+	
+	//Metodos
+
+	
+}
