1a591b1916046c7c4219e6fb0bc7ee4899c056d2
Domiidp04 <domiddp2004@gmail.com>
2023-05-23 19:53:35 +0200

    Metodos vehiculoCaro, vehiculoBarato y los dos juntos

--- Grupo2_Ejercicio2/src/concesionario/clases/Concesionario.java
+++ Grupo2_Ejercicio2/src/concesionario/clases/Concesionario.java
@@ -52,13 +52,6 @@ public class Concesionario {
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
 		// Método para buscar vehículos por marca, modelo y año
 		public List<Vehiculo> buscarVehiculos(String marca, String modelo, String anyo) {
 		    List<Vehiculo> vehiculosEncontrados = new ArrayList<>();
@@ -73,5 +66,52 @@ public class Concesionario {
 
 		    return vehiculosEncontrados;
 		}
+		
+		//Metodo para mostrar el vehiculo mas barato
+		public Vehiculo buscarVehiculoMasBarato() {
+		    Vehiculo vehiculoMasBarato = null;
+		    int precioMasBarato = Integer.MAX_VALUE;
+
+		    for (Vehiculo vehiculo : inventario) {
+		        if (!vehiculo.isEntregado() && vehiculo.getPrecio() < precioMasBarato) {
+		            vehiculoMasBarato = vehiculo;
+		            precioMasBarato = vehiculo.getPrecio();
+		        }
+		    }
+
+		    return vehiculoMasBarato;
+		}
+		
+		//Metodo para mostrar el vehiculo mas caro
+		public Vehiculo buscarVehiculoMasCaro() {
+		    Vehiculo vehiculoMasCaro = null;
+		    int precioMasCaro = 0;
+
+		    for (Vehiculo vehiculo : inventario) {
+		        if (!vehiculo.isEntregado() && vehiculo.getPrecio() > precioMasCaro) {
+		            vehiculoMasCaro = vehiculo;
+		            precioMasCaro = vehiculo.getPrecio();
+		        }
+		    }
+
+		    return vehiculoMasCaro;
+		}
+		
+		//Metodo para devolver el mas caro y el más barato
+		public void mostrarVehiculosCaroBarato() {
+	        Vehiculo vehiculoMasBarato = buscarVehiculoMasBarato();
+	        Vehiculo vehiculoMasCaro = buscarVehiculoMasCaro();
+
+	        System.out.println("Vehículo más barato:");
+            System.out.println(vehiculoMasBarato);
+
+            System.out.println("Vehículo más caro:");
+            System.out.println(vehiculoMasCaro);
+
+	    }
+		
+		
+
+
 	
 }
